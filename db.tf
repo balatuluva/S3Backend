33afedb6ed5bbcf8011e8937e963017abb57eded
balatuluva <bala.tulva@gmail.com>
2024-12-05 07:23:57 +0000

    secrets manager and also DB setup config corrections

--- db.tf
+++ db.tf
@@ -23,7 +23,7 @@ resource "aws_db_instance" "prod_db_instance" {
   allocated_storage    = 20
   storage_type         = "gp2"
   engine               = "mysql"
-  engine_version       = "5.7"
+  engine_version       = "8.0.39"
   instance_class       = "db.t2.medium"
   db_name              = "prod_db"
   username             = "adminuser"
